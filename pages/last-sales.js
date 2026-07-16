4eb03a3e832e6016bf5f5e80cfb49b19555a4a82
Code016m <miguel.r.araujo16@gmail.com>
2026-07-16 13:14:28 -0300

    Upgrade dependencies and refactor LastSalesPage to use getStaticProps for data fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-function LastSalesPage() {
-  const [sales, setSales] = useState();
+function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
   // const [isLoading, setIsLoading] = useState(false);
 
   const { data, error } = useSWR(
     "https://nextjs-course-ad988-default-rtdb.firebaseio.com/sales.json",
+    (url) => fetch(url).then((res) => res.json()),
   );
 
   useEffect(() => {
@@ -49,7 +50,7 @@ function LastSalesPage() {
     return <p>Failed to load.</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
@@ -62,6 +63,25 @@ function LastSalesPage() {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-ad988-default-rtdb.firebaseio.com/sales.json",
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return { props: { sales: transformedSales } };
 }
 
 export default LastSalesPage;
